23e7913227e43f00a8dff341811d5ec994c2f192
Abinav <abinav191510@gmail.com>
2026-03-30 11:08:16 +0530

    new changes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 app = FastAPI()
 
 @app.get("/") #defines the static route
@@ -8,3 +8,13 @@ def root():
 @app.get('/about')
 def about():
     return {'message':'New Message'}
+
+
+
+@app.get("/items/{id}")  #Dynamic route
+def get_items(id: int):
+    items = ["Keyboard", "Mouse", "Monitor","CPU"]
+
+    if id < 0 or id >= len(items):
+        raise HTTPException(status_code=404, detail = "item not found in doc")
+    return items[id]
