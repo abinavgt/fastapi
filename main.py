7eaff7f8f4ace7fa0b80a389f96e7f6f3037abbf
Abinav <abinav191510@gmail.com>
2026-03-31 19:53:18 +0530

    still developing

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Path, Query
 import json
 app = FastAPI()
 
@@ -15,18 +15,33 @@ def root():
 def about():
     return {'message':'A fully functional API to manage your patient records'}
 
-
-
-# @app.get("/items/{id}")  #Dynamic route
-# def get_items(id: int):
-#     items = ["Keyboard", "Mouse", "Monitor","CPU"]
-
-#     if id < 0 or id >= len(items):
-#         raise HTTPException(status_code=404, detail = "item not found in doc") 
-#     return items[id]
-
 @app.get('/view')
 def view():
     data = load_data()
     return data
+
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id : str = Path(..., description="ID of the patient in the DB", example='P001')):
+    data = load_data()
+
+    if patient_id in data:
+        return data[patient_id]
+    raise HTTPException(status_code=404, detail = 'Patient not found')
+
+@app.get('/sort')
+def sort_patients(sort_by: str = Query(..., description='Sort on the basis of height, weight or bmi'),
+                  order: str = Query('asc', description='Sort in asc or desc order')):
+    valid_fields = ['height', 'weight', 'bmi']
+
+
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400, detail=f'Invalid field selected. Choose from {valid_fields}')
+    
+    if order not in ['asc', 'desc']:
+        raise HTTPException(status_code=400, detail = 'Invalid order selected from asc and desc')
+    
+    data = load_data()
+    sort_order = True if order =='desc' else False
+    sorted_data = sorted(data.values(), key=lambda x: x.get(sort_by, 0),reverse = sort_order)
+    return sorted_data
     
